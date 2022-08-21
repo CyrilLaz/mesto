d3f2fa9b352516867d17b353960807c4f8ac74b1
Cyril Lazar <klazar@mail.ru>
2022-08-21 23:26:37 +1000

    fix: изменение кавычек

--- script/script.js
+++ script/script.js
@@ -1,50 +1,50 @@
 const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
   },
 ];
 
-let content = document.querySelector(".content");
+let content = document.querySelector('.content');
 
 //переменные попап профиля
-let popupProfile = document.querySelector(".popup-profile");
-let formProfile = popupProfile.querySelector(".form");
-let editButton = content.querySelector(".profile__button_type_edit");
-let addButton = content.querySelector(".profile__button_type_add");
-let name = content.querySelector(".profile__name");
-let job = content.querySelector(".profile__subname");
-let nameInput = formProfile.querySelector(".form__input_type_name");
-let jobInput = formProfile.querySelector(".form__input_type_job");
-let closeButtonProfile = popupProfile.querySelector(".popup__button-close");
+let popupProfile = document.querySelector('.popup-profile');
+let formProfile = popupProfile.querySelector('.form');
+let editButton = content.querySelector('.profile__button_type_edit');
+let addButton = content.querySelector('.profile__button_type_add');
+let name = content.querySelector('.profile__name');
+let job = content.querySelector('.profile__subname');
+let nameInput = formProfile.querySelector('.form__input_type_name');
+let jobInput = formProfile.querySelector('.form__input_type_job');
+let closeButtonProfile = popupProfile.querySelector('.popup__button-close');
 
 //переменные попап добавления фотографий
-let popupAddPicture = document.querySelector(".popup-addPicture");
-let formAddPicture = popupAddPicture.querySelector(".form");
-let titleInput = popupAddPicture.querySelector(".form__input_type_title");
-let urlInput = popupAddPicture.querySelector(".form__input_type_url");
+let popupAddPicture = document.querySelector('.popup-addPicture');
+let formAddPicture = popupAddPicture.querySelector('.form');
+let titleInput = popupAddPicture.querySelector('.form__input_type_title');
+let urlInput = popupAddPicture.querySelector('.form__input_type_url');
 let closeButtonAddPicture = popupAddPicture.querySelector(
-  ".popup__button-close"
+  '.popup__button-close'
 );
 
 //функция открытия попап профиля
@@ -52,15 +52,15 @@ function openPopupProfile() {
   jobInput.value = job.textContent;
   nameInput.value = name.textContent;
 
-  popupProfile.classList.add("popup_opened");
+  popupProfile.classList.add('popup_opened');
 }
 
 //функция обработчик событий
 const eventProssesor = (elementItem) => {
-  elementItem.addEventListener("click", (evt) => {
-    if (evt.target === elementItem.querySelector(".cards__like-icon"))
+  elementItem.addEventListener('click', (evt) => {
+    if (evt.target === elementItem.querySelector('.cards__like-icon'))
       liked(elementItem);
-    else if (evt.target === elementItem.querySelector(".cards__button-delete"))
+    else if (evt.target === elementItem.querySelector('.cards__button-delete'))
       deletePicture(elementItem);
     else openPicture(elementItem);
   });
@@ -68,13 +68,13 @@ const eventProssesor = (elementItem) => {
 
 //функция создания карточки
 const createCard = (url, title) => {
-  let cardTemplate = document.querySelector("#card-template").content;
-  let cardsList = document.querySelector(".cards__list");
+  let cardTemplate = document.querySelector('#card-template').content;
+  let cardsList = document.querySelector('.cards__list');
 
-  let cardItem = cardTemplate.querySelector("li").cloneNode(true);
+  let cardItem = cardTemplate.querySelector('li').cloneNode(true);
 
-  cardItem.querySelector(".cards__picture").src = url;
-  cardItem.querySelector(".cards__title").textContent = title;
+  cardItem.querySelector('.cards__picture').src = url;
+  cardItem.querySelector('.cards__title').textContent = title;
   cardsList.append(cardItem);
 
   eventProssesor(cardItem); //обработчик событий
@@ -82,28 +82,28 @@ const createCard = (url, title) => {
 
 //функция создания попап фотографии
 const openPicture = (elementItem) => {
-  let pictureTemplate = document.querySelector("#popup-template").content;
+  let pictureTemplate = document.querySelector('#popup-template').content;
 
   let popupPicture = pictureTemplate
-    .querySelector(".popup-picture")
+    .querySelector('.popup-picture')
     .cloneNode(true);
 
-  popupPicture.querySelector(".popup-picture__picture").src =
-    elementItem.querySelector(".cards__picture").src;
-  popupPicture.querySelector(".popup-picture__title").textContent =
-    elementItem.querySelector(".cards__title").textContent;
+  popupPicture.querySelector('.popup-picture__picture').src =
+    elementItem.querySelector('.cards__picture').src;
+  popupPicture.querySelector('.popup-picture__title').textContent =
+    elementItem.querySelector('.cards__title').textContent;
   content.append(popupPicture);
 
   //слушатель на закрытие
   popupPicture
-    .querySelector(".popup__button-close")
-    .addEventListener("click", () => popupPicture.remove());
+    .querySelector('.popup__button-close')
+    .addEventListener('click', () => popupPicture.remove());
 };
 
 //функция изменния лайка
 function liked(elementItem) {
-  let likeIcon = elementItem.querySelector(".cards__like-icon");
-  likeIcon.classList.toggle("cards__like-icon_active");
+  let likeIcon = elementItem.querySelector('.cards__like-icon');
+  likeIcon.classList.toggle('cards__like-icon_active');
 }
 
 //добавление фотографий из массива
@@ -121,7 +121,7 @@ function editInfo(evt) {
 
 // функция открытия папап добавления фотографии
 function openPopupAddPicture() {
-  popupAddPicture.classList.add("popup_opened");
+  popupAddPicture.classList.add('popup_opened');
 }
 
 // функция добавления фотографии из попапа
@@ -132,8 +132,8 @@ function addPicture(evt) {
 
   closePopup(evt);
 
-  urlInput.value = "";
-  titleInput.value = "";
+  urlInput.value = '';
+  titleInput.value = '';
 }
 
 //функция удаления карточки
@@ -142,15 +142,15 @@ function deletePicture(elementItem) {
 }
 
 function closePopup(evt) {
-  evt.target.closest(".popup").classList.remove("popup_opened");
+  evt.target.closest('.popup').classList.remove('popup_opened');
 }
 
 // слушатели на попап-профиле
-editButton.addEventListener("click", openPopupProfile);
-closeButtonProfile.addEventListener("click", closePopup);
-formProfile.addEventListener("submit", editInfo);
+editButton.addEventListener('click', openPopupProfile);
+closeButtonProfile.addEventListener('click', closePopup);
+formProfile.addEventListener('submit', editInfo);
 
 // слушатели на попап-добавлении фотографий
-addButton.addEventListener("click", openPopupAddPicture);
-closeButtonAddPicture.addEventListener("click", closePopup);
-formAddPicture.addEventListener("submit", addPicture);
+addButton.addEventListener('click', openPopupAddPicture);
+closeButtonAddPicture.addEventListener('click', closePopup);
+formAddPicture.addEventListener('submit', addPicture);
